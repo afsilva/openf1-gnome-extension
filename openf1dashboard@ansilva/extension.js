4dcced8b266e2241d600181a87fd75d74c4d1cca
afsilva <afsilva@users.noreply.github.com>
2026-08-30 13:05:19 -0400

    chore: update build marker for standings fix

--- openf1dashboard@ansilva/extension.js
+++ openf1dashboard@ansilva/extension.js
@@ -23,7 +23,7 @@ const MAX_STANDINGS_API_SESSIONS_PER_REFRESH = 4;
 const ALLOWED_ENDPOINTS = new Set(['meetings', 'sessions', 'session_result', 'drivers']);
 const UI_SCHEMA_VERSION = 6;
 const BUILD_VERSION = '7';
-const BUILD_COMMIT = 'standings-rate-limit-fix';
+const BUILD_COMMIT = '1143e42';
 
 const _unknownCountryCodesLogged = new Set();
 
